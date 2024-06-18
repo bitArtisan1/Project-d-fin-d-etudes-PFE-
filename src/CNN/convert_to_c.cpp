3d6664a6e355adbd1b7582bb072bf71ecaa00ea1
Aamar <121260820+git-aamar@users.noreply.github.com>
2024-06-18 17:13:03 +0100

    Delete src/CNN/convert_to_c.cpp

--- src/CNN/convert_to_c.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdlib>
-
-int main() {
-    // Run the xxd command to convert the TFLite model to C code
-    std::system("xxd -i C:\\Users\\Aamar\\Documents\\MATLAB\\Examples\\R2023b\\deeplearning_shared\\KeywordSpottingInNoiseUsingMFCCAndLSTMNetworksExample\\CNN\\smaller_optimized_model.tflite > model_data.cc");
-
-    return 0;
-}
